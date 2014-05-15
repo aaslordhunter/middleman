06dfc295ffbd03dc081f172507c8d189b430fa2b
Ali Aas <aliaas@Alis-MacBook-Air.local>
2014-05-15 07:27:16 -0400

    remove minifying, fix layout

--- config.rb
+++ config.rb
@@ -1,12 +1,7 @@
 activate :i18n
-activate :minify_html
 activate :relative_assets
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :relative_links, true
-
-configure :build do
-  activate :minify_css
-end
